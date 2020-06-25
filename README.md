4b5552cdfe5b587711e4639f89df947526bf5bfc
AurelienNioche <nioche.aurelien@gmail.com>
2020-06-25 17:27:25 +0300

    update

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ N lines: 136,368,882
 N user = 23,512
 
 N item = 18,438,496
+
+N user item pairs = 21,249,531
--- basic_stats.py
+++ basic_stats.py
@@ -8,22 +8,22 @@ from tqdm import tqdm
 import numpy as np
 from data_interface.models import Log
 
-N_USER = 23512
-
-n = N_USER
-users = Log.objects.distinct('user_id').values_list('user_id', flat=True)
-counts = {
-    "user_id": np.zeros(n, dtype=str),
-    "count": np.zeros(n)
-}
-for i, u in tqdm(enumerate(users), total=n):
-    c = Log.objects.filter(user_id=u).count()
-    counts["count"][i] = c
-    counts["user_id"][i] = u
-print("backing up...", flush=True, end=" ")
-df = pd.DataFrame(counts)
-df.to_csv('counts_user.csv')
-print("done!")
+# N_USER = 23512
+#
+# n = N_USER
+# users = Log.objects.distinct('user_id').values_list('user_id', flat=True)
+# counts = {
+#     "user_id": np.zeros(n, dtype=str),
+#     "count": np.zeros(n)
+# }
+# for i, u in tqdm(enumerate(users), total=n):
+#     c = Log.objects.filter(user_id=u).count()
+#     counts["count"][i] = c
+#     counts["user_id"][i] = u
+# print("backing up...", flush=True, end=" ")
+# df = pd.DataFrame(counts)
+# df.to_csv('counts_user.csv')
+# print("done!")
 
 v = Log.objects.distinct("user_id", "object_id").values_list("user_id", "object_id")
 n = len(v)
